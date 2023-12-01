d6963e6c76e2abba6263400fc546ec9caa046763
Cameron Pittman <cameron.w.pittman@gmail.com>
2023-12-01 16:33:42 -0500

    finish day 1 part 2

--- 2023/day1.cpp
+++ 2023/day1.cpp
@@ -90,23 +90,37 @@ int find_number_in_words(char c, std::vector<std::string> *maybe_words) {
                                         "four",  "five", "six", "seven",
                                         "eight", "nine"};
 
+  // grow all word strings
   for (std::string &maybe_word : *maybe_words) {
     maybe_word.push_back(c);
   }
 
+  // and make a new word string starting with this char
   std::string new_word(1, c);
   maybe_words->push_back(new_word);
 
+  // check if any word string matches a number
   for (std::string &maybe_word : *maybe_words) {
-    for (std::string &number_word : number_words) {
-      if (maybe_word == number_word) {
-        // TODO iterate so we get the place of the number word too
-        std::cout << maybe_word << std::endl;
+    for (int i = 0; i < number_words.size(); i++) {
+      if (maybe_word == number_words[i]) {
+
+        // as pointed out on reddit:
+        // "The right calibration values for string "eighthree" is 83 and for
+        // "sevenine" is 79."
+
+        // so we can still clear everything...
+        maybe_words->clear();
+
+        // ... but we should keep the character we just got
+        std::string new_word(1, c);
+        maybe_words->push_back(new_word);
+
+        return i;
       }
     }
   }
 
-  return 0;
+  return -1;
 }
 
 std::string Day1::part2() {
@@ -155,10 +169,9 @@ void Day1::run(int part) {
   }
 
   std::cout << "Day " << thisDay;
-  std::cout << " part 2: " << part2() << std::endl;
-  // if (part == 1) {
-  //   std::cout << " part 1: " << part1() << std::endl;
-  // } else {
-  //   std::cout << " part 2: " << part2() << std::endl;
-  // };
+  if (part == 1) {
+    std::cout << " part 1: " << part1() << std::endl;
+  } else {
+    std::cout << " part 2: " << part2() << std::endl;
+  };
 }
